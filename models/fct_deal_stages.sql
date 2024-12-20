cf9ba06a84b67f0abf9a85ac7417ef6d8378e587
Alexey Nikolaev <alexey_nikolaev@ymail.com>
2024-12-20 23:00:19 +0100

    fct_deal_stages

--- models/fct_deal_stages.sql
+++ models/fct_deal_stages.sql
@@ -1,6 +1,8 @@
 SELECT
+    HASHTEXT(deal_id || '_' || new_value) AS id,
     deal_id,
     CAST(new_value AS INTEGER) AS stage_id,
-    change_time AS stage_entered_timestamp
+    MIN(change_time) AS stage_first_entered_timestamp
 FROM {{ ref('stg_deal_changes') }}
 WHERE changed_field_key = 'stage_id'
+GROUP BY 1, 2, 3
